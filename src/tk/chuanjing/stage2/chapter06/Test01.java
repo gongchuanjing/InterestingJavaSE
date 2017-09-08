4c2ad24b208c728dbb23f9ed038710f4adad4b38
gcj <longgongsanri@163.com>
2017-09-08 22:17:57 +0800

    演示错误

---
+++ src/tk/chuanjing/stage2/chapter06/Test01.java
@@ -0,0 +1,27 @@
+package tk.chuanjing.stage2.chapter06;
+/**
+ * @author ChuanJing
+ * @date 2017年9月8日 下午9:42:23
+ * @version 1.0
+ */
+public class Test01 {
+
+	public static void main(String[] args) {
+		T1 t1 = new T1("张三0", 99);
+	}
+
+}
+
+class T2 {
+	public T2() {
+		
+	}
+	
+	public T2(String s, int a) {
+		
+	}
+}
+
+class T1 extends T2 {
+	
+}
