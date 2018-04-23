92c9cb4997badf12740b102c44e53f380b76b913
gcj <longgongsanri@163.com>
2018-04-23 19:33:28 +0800

    测试

---
+++ src/tk/chuanjing/stage2/chapter05/homework/HW07.java
@@ -0,0 +1,23 @@
+package tk.chuanjing.stage2.chapter05.homework;
+/**
+ * @author ChuanJing
+ * @date 2018年4月20日 下午7:26:51
+ * @version 1.0
+
+分别写出八种基本数据类型对应的包装类
+ */
+public class HW07 {
+
+	public static void main(String[] args) {
+		
+		Byte b;
+		Short s;
+		Integer i ;
+		Long l ;
+		
+		Boolean bb;
+		Double d;
+		Character c;
+		Float f;
+	}
+}
