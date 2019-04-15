5220ffba5655dc45ea7483b757109f3f198d9596
gcj <longgongsanri@163.com>
2019-04-15 15:10:23 +0800

    Lambda表达式

---
+++ src/tk/chuanjing/stage2/chapter11/textbook/LambdaDemo02.java
@@ -0,0 +1,79 @@
+package tk.chuanjing.stage2.chapter11.textbook;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+    Lambda表达式有参数有返回值的练习
+    需求:
+        使用数组存储多个Person对象
+        对数组中的Person对象使用Arrays的sort方法通过年龄进行升序排序
+ */
+public class LambdaDemo02 {
+    public static void main(String[] args) {
+        //使用数组存储多个Person对象
+        Person[] arr = {
+                new Person("柳岩",38),
+                new Person("迪丽热巴",18),
+                new Person("古力娜扎",19)
+        };
+
+        //对数组中的Person对象使用Arrays的sort方法通过年龄进行升序(前边-后边)排序
+        /*Arrays.sort(arr, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getAge()-o2.getAge();
+            }
+        });*/
+        
+        //使用Lambda表达式,简化匿名内部类
+        Arrays.sort(arr,(Person o1, Person o2)->{
+            return o1.getAge()-o2.getAge();
+        });
+
+        //优化省略Lambda
+        Arrays.sort(arr,(o1, o2)->o1.getAge()-o2.getAge());
+
+        //遍历数组
+        for (Person p : arr) {
+            System.out.println(p);
+        }
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
